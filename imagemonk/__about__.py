548a5c8b1d078e84146d1bc711b64042ece6659b
Ethan Paul <24588726+enpaul@users.noreply.github.com>
2020-09-21 23:12:24 -0400

    Bump feature version
    Fix mixmatched meta

--- imagemonk/__about__.py
+++ imagemonk/__about__.py
@@ -2,7 +2,7 @@
 
 
 __title__ = "imagemonk"
-__version__ = "0.0.0"
+__version__ = "0.1.0"
 __authors__ = ["Ethan Paul <24588726+enpaul@users.noreply.github.com>"]
 __license__ = "MIT"
 __summary__ = "HTTP server for uploading images and generating thumbnails"
