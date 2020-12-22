1c1de0c241094e949393eebb4f6fe3740d3baaa8
Ethan Paul <24588726+enpaul@users.noreply.github.com>
2020-12-21 21:51:20 -0500

    Fix missing repo metadata
    Add missing entries to __about__
    Add missing tests/__init__ file
    Add missing tests to test_about
    Fix trailing whitespace in openapi

--- imagemonk/__about__.py
+++ imagemonk/__about__.py
@@ -1,6 +1,9 @@
 """Programatically accessible project metadata"""
 
 
-__title__ = "awesome-template"
+__title__ = "imagemonk"
 __version__ = "0.0.0"
-__authors__ = ["Ethan Paul <e@enp.one>"]
+__authors__ = ["Ethan Paul <24588726+enpaul@users.noreply.github.com>"]
+__license__ = "MIT"
+__summary__ = "HTTP server for uploading images and generating thumbnails"
+__url__ = "https://github.com/mocproject/imagemonk/"
--- tests/test_about.py
+++ tests/test_about.py
@@ -9,14 +9,25 @@ from imagemonk import __about__
 def test_about():
     """Test metadata values"""
 
-    with (Path(__file__).parent.parent / "pyproject.toml").open() as infile:
-        about = toml.load(infile)
+    with (Path(__file__).resolve().parent.parent / "pyproject.toml").open() as infile:
+        pyproject = toml.load(infile)
 
-    assert about["tool"]["poetry"]["name"] == __about__.__title__
-    assert about["tool"]["poetry"]["version"] == __about__.__version__
-    assert all(
-        author in about["tool"]["poetry"]["authors"] for author in __about__.__authors__
+    assert pyproject["tool"]["poetry"]["name"] == __about__.__title__
+    assert pyproject["tool"]["poetry"]["version"] == __about__.__version__
+    assert pyproject["tool"]["poetry"]["license"] == __about__.__license__
+    assert pyproject["tool"]["poetry"]["description"] == __about__.__summary__
+    assert pyproject["tool"]["poetry"]["repository"] == __about__.__url__
+    assert (
+        all(
+            item in __about__.__authors__
+            for item in pyproject["tool"]["poetry"]["authors"]
+        )
+        is True
     )
-    assert all(
-        author in __about__.__authors__ for author in about["tool"]["poetry"]["authors"]
+    assert (
+        all(
+            item in pyproject["tool"]["poetry"]["authors"]
+            for item in __about__.__authors__
+        )
+        is True
     )
