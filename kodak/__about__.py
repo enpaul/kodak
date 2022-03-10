1ace9ae8810201cac09dfc547401f4f25d1483dc
Ethan Paul <24588726+enpaul@users.noreply.github.com>
2022-03-09 19:54:17 -0500

    Bump bugfix version

--- kodak/__about__.py
+++ kodak/__about__.py
@@ -2,7 +2,7 @@
 
 
 __title__ = "kodak"
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __authors__ = ["Ethan Paul <24588726+enpaul@users.noreply.github.com>"]
 __license__ = "MIT"
 __summary__ = "HTTP server for uploading images and generating thumbnails"
